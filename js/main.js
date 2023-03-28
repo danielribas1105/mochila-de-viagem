6d5db8bf1bedee382ca3356fbe1abeb53b9818b5
Daniel <danielribas1105@gmail.com>
2023-03-28 15:15:04 -0300

    update main js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,52 @@
 
 const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
-const listaItens = [];
+const listaItens = JSON.parse(localStorage.getItem('listaItens')) || [];
 
-function criarElemento(nome, quantidade){
+function criarElemento(itemAdd){
     const novoItem = document.createElement('li');
     novoItem.classList.add('item');
     const quantItem = document.createElement('strong');
-    quantItem.innerHTML = quantidade;
+    quantItem.innerHTML = itemAdd.quantidade;
+    quantItem.dataset.id = itemAdd.id;
     novoItem.appendChild(quantItem);
-    novoItem.innerHTML += nome;
+    novoItem.innerHTML += itemAdd.nome;
     lista.appendChild(novoItem);
+}
 
-    const itemAtual = {
-        'nome': nome,
-        'quantidade': quantidade
-    };
-    listaItens.push(itemAtual);
-    localStorage.setItem('listaItens', JSON.stringify(listaItens));
-    
+function atualizarElemento(item){
+    document.querySelector('[data-id="'+item.id+'"]').innerHTML += Number(item.quantidade);
+
 }
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
-    criarElemento(nome.value, quantidade.value);
+    const itemAtual = {
+        'nome': nome.value,
+        'quantidade': quantidade.value
+    };
+    const existeItem = listaItens.find(elemento => elemento.nome === nome.value);
+    
+    if(existeItem){
+        itemAtual.id = existeItem.id;
+        atualizarElemento(itemAtual);
+    }else{
+        itemAtual.id = listaItens.length;
+        criarElemento(itemAtual);
+        listaItens.push(itemAtual);
+    }
+
+
+    localStorage.setItem('listaItens', JSON.stringify(listaItens));
+
     nome.value = "";
     quantidade.value = "";
     
+});
+
+listaItens.forEach((elemento) => {
+    criarElemento(elemento);
 });
 
