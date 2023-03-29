ea57f33176be8d270c58a9e43ea5f02a46702d87
Daniel <danielribas1105@gmail.com>
2023-03-28 22:14:28 -0300

    update main js

--- js/main.js
+++ js/main.js
@@ -12,11 +12,26 @@ function criarElemento(itemAdd){
     novoItem.appendChild(quantItem);
     novoItem.innerHTML += itemAdd.nome;
     lista.appendChild(novoItem);
+    novoItem.appendChild(btnDeletar(itemAdd.id));
 }
 
 function atualizarElemento(item){
-    document.querySelector('[data-id="'+item.id+'"]').innerHTML += Number(item.quantidade);
+    document.querySelector('[data-id="'+item.id+'"]').innerHTML = item.quantidade;
+}
 
+function btnDeletar(id){
+    const elementoBtn = document.createElement('button');
+    elementoBtn.innerText = "X";
+    elementoBtn.addEventListener('click', function() {
+        deletarElemento(this.parentNode, id);
+    });
+    return elementoBtn;
+}
+
+function deletarElemento(tag, id){
+    tag.remove();
+    listaItens.splice(listaItens.findIndex(elemento => elemento.id === id), 1);
+    localStorage.setItem('listaItens', JSON.stringify(listaItens));
 }
 
 form.addEventListener('submit', (evento) => {
@@ -32,18 +47,17 @@ form.addEventListener('submit', (evento) => {
     if(existeItem){
         itemAtual.id = existeItem.id;
         atualizarElemento(itemAtual);
+        listaItens[listaItens.findIndex(elemento => elemento.id === existeItem.id)] = itemAtual;
     }else{
-        itemAtual.id = listaItens.length;
+        itemAtual.id = listaItens[listaItens.length - 1] ? 
+                                (listaItens[listaItens.length - 1]).id + 1 : 0 ;
         criarElemento(itemAtual);
         listaItens.push(itemAtual);
     }
 
-
     localStorage.setItem('listaItens', JSON.stringify(listaItens));
-
     nome.value = "";
     quantidade.value = "";
-    
 });
 
 listaItens.forEach((elemento) => {
